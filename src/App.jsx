4aaf23b24fd6a03b83c22e93ad08b2651e7315b1
MinhalCoder <minhalmehtab561@gmail.com>
2024-07-22 08:47:46 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './Pages/HomePages/HomePage';
 import Circle from './Components/Circle/Circle';
 import ModeToggle from './Components/ModeToggle';
+import NeonCursor from './Components/NeonCursor';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <>
       <HomePage/>
       <ModeToggle/>
+      <NeonCursor/>
     </>
   )
 }
